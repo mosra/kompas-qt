d5f399c6470d1f96adf03237bd9d847a047a1fd5
Vladimír Vondruš <mosra@centrum.cz>
2010-07-15 16:40:58 +0200

    Dummy MainWindow.

---
+++ src/MainWindow.cpp
@@ -0,0 +1,58 @@
+/*
+    Copyright © 2007, 2008, 2009, 2010 Vladimír Vondruš <mosra@centrum.cz>
+
+    This file is part of Map2X.
+
+    Map2X is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License version 3
+    only, as published by the Free Software Foundation.
+
+    Map2X is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License version 3 for more details.
+*/
+
+#include <QtGui/QApplication>
+#include <QtGui/QMenuBar>
+#include <QtGui/QStyle>
+
+#include "MainWindow.h"
+
+namespace Map2X { namespace QtGui {
+
+MainWindow::MainWindow(QWidget* parent, Qt::WindowFlags flags): QMainWindow(parent, flags) {
+    setWindowTitle("Map2X");
+
+    createActions();
+    createMenus();
+
+    statusBar();
+
+    resize(800, 600);
+}
+
+void MainWindow::createActions() {
+    /* Quit application */
+    quitAction = new QAction(tr("Quit"), this);
+    quitAction->setShortcut(QKeySequence::Quit);
+    quitAction->setStatusTip(tr("Quit application"));
+    connect(quitAction, SIGNAL(triggered(bool)), SLOT(close()));
+
+    /* About Qt */
+    aboutQtAction = new QAction(QIcon(":/trolltech/qmessagebox/images/qtlogo-64.png"), tr("About Qt"), this);
+    aboutQtAction->setStatusTip(tr("Show information about Qt"));
+    connect(aboutQtAction, SIGNAL(triggered(bool)), qApp, SLOT(aboutQt()));
+}
+
+void MainWindow::createMenus() {
+    /* File menu */
+    QMenu* fileMenu = menuBar()->addMenu(tr("File"));
+    fileMenu->addAction(quitAction);
+
+    /* Help menu */
+    QMenu* helpMenu = menuBar()->addMenu(tr("Help"));
+    helpMenu->addAction(aboutQtAction);
+}
+
+}}
---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+/*
+    Copyright © 2007, 2008, 2009, 2010 Vladimír Vondruš <mosra@centrum.cz>
+
+    This file is part of Map2X.
+
+    Map2X is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License version 3
+    only, as published by the Free Software Foundation.
+
+    Map2X is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License version 3 for more details.
+*/
+
+#include <QtCore/QTextCodec>
+#include <QtCore/QTranslator>
+#include <QtCore/QLocale>
+#include <QtCore/QLibraryInfo>
+#include <QtGui/QApplication>
+
+#include "MainWindow.h"
+
+int main(int argc, char** argv) {
+    QApplication app(argc, argv);
+    app.setApplicationName("Map2X");
+    /** @todo Organization? */
+    app.setOrganizationName("Mosra");
+
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+
+    /* Localizations */
+    QTranslator translatorQt, translator;
+    translatorQt.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.installTranslator(&translatorQt);
+
+    /* Main window */
+    Map2X::QtGui::MainWindow w;
+    w.show();
+
+    return app.exec();
+}

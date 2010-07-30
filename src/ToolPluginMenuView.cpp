97dce74683adc7f53d9b023739ce263e20c77997
Vladimír Vondruš <mosra@centrum.cz>
2010-07-30 21:47:51 +0200

    Viewer for tool plugins in menu.

---
+++ src/ToolPluginMenuView.cpp
@@ -0,0 +1,58 @@
+/*
+    Copyright © 2007, 2008, 2009, 2010 Vladimír Vondruš <mosra@centrum.cz>
+
+    This file is part of Map2X.
+
+    Map2X is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License version 3
+    only, as published by the Free Software Foundation.
+
+    Map2X is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License version 3 for more details.
+*/
+
+#include "ToolPluginMenuView.h"
+
+#include <QtGui/QMenu>
+#include <QtGui/QAction>
+
+using namespace std;
+using namespace Map2X::PluginManager;
+
+namespace Map2X { namespace QtGui {
+
+ToolPluginMenuView::ToolPluginMenuView(MainWindow* _mainWindow, PluginManager::PluginManager<AbstractTool>* _manager, QMenu* _menu, QAction* _before, QObject* parent): QObject(parent), mainWindow(_mainWindow), manager(_manager), menu(_menu), before(_before) { update(); }
+
+void ToolPluginMenuView::update() {
+    if(!items.isEmpty()) {
+        qDeleteAll<QList<QAction*> >(items.keys());
+        items.clear();
+    }
+
+    /* Foreach plugin list and create menu items */
+    vector<string> plugins = manager->nameList();
+
+    for(vector<string>::const_iterator it = plugins.begin(); it != plugins.end(); ++it) {
+        /* Skip not loaded plugins */
+        if(!(manager->loadState(*it) & (AbstractPluginManager::LoadOk|AbstractPluginManager::IsStatic)))
+            continue;
+
+        AbstractTool* instance = manager->instance(*it);
+        QAction* action = new QAction(instance->menuIcon(), instance->menuText(), mainWindow);
+        connect(action, SIGNAL(triggered(bool)), this, SLOT(openToolDialog()));
+
+        menu->insertAction(before, action);
+        items.insert(action, instance);
+    }
+}
+
+void ToolPluginMenuView::openToolDialog() {
+    /* Instantiate dialog */
+    AbstractToolDialog* dialog = items.value(qobject_cast<QAction*>(sender()))->create(mainWindow, mainWindow);
+    dialog->exec();
+    delete dialog;
+}
+
+}}
---
+++ src/ToolPluginMenuView.h
@@ -0,0 +1,76 @@
+#ifndef Map2X_QtGui_ToolPluginMenuView_h
+#define Map2X_QtGui_ToolPluginMenuView_h
+/*
+    Copyright © 2007, 2008, 2009, 2010 Vladimír Vondruš <mosra@centrum.cz>
+
+    This file is part of Map2X.
+
+    Map2X is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License version 3
+    only, as published by the Free Software Foundation.
+
+    Map2X is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License version 3 for more details.
+*/
+
+/** @file
+ * @brief Class Map2X::QtGui::ToolPluginMenuView
+ */
+
+#include "PluginManager/PluginManager.h"
+
+#include <QtCore/QObject>
+#include <QtCore/QHash>
+
+#include "MainWindow.h"
+#include "AbstractTool.h"
+
+class QMenu;
+class QAction;
+
+namespace Map2X { namespace QtGui {
+
+/**
+ * @brief Menu view of tool plugins
+ *
+ * Fills up given menu with loaded tool plugins.
+ */
+class ToolPluginMenuView: public QObject {
+    Q_OBJECT
+
+    public:
+        /**
+         * @brief Constructor
+         * @param _mainWindow   Pointer to main window
+         * @param _manager      Plugin manager with tools
+         * @param _menu         Menu to which insert tools
+         * @param _before       Menu item before which insert tools
+         * @param parent        Parent object
+         */
+        ToolPluginMenuView(MainWindow* _mainWindow, PluginManager::PluginManager<AbstractTool>* _manager, QMenu* _menu, QAction* _before = 0, QObject* parent = 0);
+
+    public slots:
+        /**
+         * @brief Update plugin list
+         *
+         * Reloads menu with actual plugin list.
+         */
+        void update();
+
+    private slots:
+        /** @todo Open as modeless dialog */
+        void openToolDialog();
+
+    private:
+        MainWindow* mainWindow;
+        PluginManager::PluginManager<AbstractTool>* manager;
+        QMenu* menu;
+        QAction* before;
+        QHash<QAction*, AbstractTool*> items;
+};
+
+}}
+
+#endif
